3a4973cad139221f2aa0a6af27da4be4af25fbae
Yudha Satria <yudha.satria.0890@gmail.com>
2024-01-17 15:23:35 +0700

    feat : change directory

---
+++ Commands/ChangeDirectories.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment_scanner.Commands
+{
+    public static class ChangeDirectories
+    {
+        public static string CurrentPath { get; set; }
+        
+        public static void ChangeDirectory(string path)
+        {
+            if (path == "..")
+            {
+                CurrentPath = Path.GetPathRoot(Environment.SystemDirectory);
+            }
+            else
+            {
+                CurrentPath = string.IsNullOrEmpty(CurrentPath) ? Path.GetPathRoot(Environment.SystemDirectory) : CurrentPath;
+
+                if (Directory.Exists(Path.Combine(CurrentPath, path)))
+                {
+                    CurrentPath = Path.Combine(CurrentPath, path);
+                }
+                else
+                {
+                    Console.WriteLine("path not found");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using assessment_scanner.Commands;
+using System.Runtime.CompilerServices;
+
+string command = "";
+List<string> commandDetail = new List<string>();
+
+Console.WriteLine("Assessmemnt Scanner");
+
+while (command != "exit")
+{
+    Console.WriteLine();
+    Console.Write((string.IsNullOrEmpty(ChangeDirectories.CurrentPath) ? Path.GetPathRoot(Environment.SystemDirectory) : ChangeDirectories.CurrentPath) + " >> ");
+    command = Console.ReadLine();
+    commandDetail = command.Split(' ').ToList();
+
+    if (commandDetail[0] == "cd")
+    {
+        ChangeDirectories.ChangeDirectory(commandDetail[1]);
+    }
+    else if (commandDetail[0] == "exit")
+    { 
+        
+    }
+    else
+    {
+        Console.WriteLine("Command not found");
+    }
+}
